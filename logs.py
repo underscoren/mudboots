0e39791051ad7da47cf4dca95cf7bd87df46aa87
underscoren <marius.steffens@outlook.com>
2024-05-09 12:46:02 +0100

    fix linux home dir path v2

--- logs.py
+++ logs.py
@@ -5,6 +5,6 @@ hackmudDir = ""
 if platform == "win32":
     hackmudDir = path.join(environ["APPDATA"],"hackmud")
 elif platform == "linux":
-    hackmudDir = path.join(path.expanduser("~"),"/.config/hackmud/")
+    hackmudDir = path.join(path.expanduser("~"),".config/hackmud")
 
 shelltxt = path.join(hackmudDir, "shell.txt")
